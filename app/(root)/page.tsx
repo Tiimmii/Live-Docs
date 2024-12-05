82764f34787adcde79aea0166d43d09e114e1067
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-12-05 22:36:02 +0100

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+//make sure to install shadn and add button component
+//npm i jsm-editor
+//npx jsm-editor add editor
+
+const page = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default page
