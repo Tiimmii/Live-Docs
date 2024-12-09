33b3f5a38087a50e26690fa814a29c1bd43a3fa3
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-12-09 22:35:45 +0100

    listing documents on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,15 +7,20 @@ import { redirect } from 'next/navigation'
 import Image from 'next/image'
 import React from 'react'
 import { currentUser } from '@clerk/nextjs/server'
+import { getDocuments } from '@/lib/actions/rooms.action'
+import { metadata } from '../layout'
+import Link from 'next/link'
+import { dateConverter } from '@/lib/utils'
 
 //make sure to install shadn and add button component
 //npm i jsm-editor
 //npx jsm-editor add editor
 
 const page = async () => {
-  const documents = []
   const clerkUser = await currentUser();
   if(!clerkUser) redirect('sign-in'); 
+
+  const roomDocuments = await getDocuments(clerkUser.emailAddresses[0].emailAddress)
 
   return (
     <main className='home-container'>
@@ -27,9 +32,32 @@ const page = async () => {
           </SignedIn>
         </div>
       </Header>
-      {documents.length>0?(
-        <div>
-
+      {roomDocuments.data.length>0?(
+        <div className='document-list-container'>
+          <div className='document-list-title'>
+            <h3 className='text-28-semibold'>All documents</h3>
+            <AddDocumentBtn userId={clerkUser.id} email={clerkUser.emailAddresses[0].emailAddress}/>
+          </div>
+          <ul className='document-ul'>
+            {roomDocuments.data.map(({ id, metadata, createdAt }: any)=>(
+              <li key={id} className='document-list-item'>
+                <Link href={`/documents/${id}`} className='flex flex-1 items-center gap-4'>
+                  <div className='hidden rounded-md bg-dark-500 p-2 sm:block'>
+                    <Image
+                      src={"/assets/icons/document.svg"}
+                      alt='file'
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div className='space-y-1'>
+                    <p className='line-clamp-1 text-lg'>{metadata.title}</p>
+                    <p className='text-sm font-light text-blue-100'>Created about {dateConverter(createdAt)}</p>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
         ):(
         <div className='document-list-empty'>
